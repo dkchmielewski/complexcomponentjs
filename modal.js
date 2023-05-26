27b5f8234a10549a6d0cfdab1d766da693512229
dkchmielewski <dkchmielewski@gmail.com>
2023-05-25 23:08:56 -0400

    listening to slot content changes

--- modal.js
+++ modal.js
@@ -44,7 +44,7 @@ class Modal extends HTMLElement {
                     padding: 1rem;
                 }
 
-                header h1 {
+                ::slotted(h1) {
                     font-size: 1.25rem;
                 }
 
@@ -66,7 +66,7 @@ class Modal extends HTMLElement {
             <div id="backdrop"></div>
             <div id="modal">
                 <header>
-                    <h1>Please Confirm</h1>
+                    <slot name="title">Please Confirm Payment</slot>
                 </header>
                 <section id="main">
                     <slot></slot>
@@ -77,6 +77,10 @@ class Modal extends HTMLElement {
                 </section>
             </div>
         `;
+        const slots = this.shadowRoot.querySelectorAll('slot');
+        slots[1].addEventListener('slotchange', event => {
+            console.dir(slots[1].assignedNodes());
+        });
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
